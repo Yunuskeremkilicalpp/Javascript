9a4229de2ded3b70deeaa4bc7d07e121fcb4ab13
unknown <Yunus@MSI-A01341.mps.k12>
2022-09-02 11:59:16 -0800

    hello

--- js-basics/index.js
+++ js-basics/index.js
@@ -1,1 +1,19 @@
-    console.log("hello world")
+        // This is my first JavaScript code!
+ /*       console.log("hello world")
+    let name = 'Yunus';
+//    console.log(name) ;
+*/
+    // cannot be a reserved keyword
+    // Should be meaningful
+    // Cannot start with a number (1name)
+    // Cannot contain a space or hyphen (-)
+    // Aere case-sensitive 
+
+    let person = {
+        name: "Yunus",
+        age: 16
+    };
+
+    console.log(person) ; 
+
+    
