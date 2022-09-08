a80553f51ad4a2b085c7c9220a574c4ae48af5e7
unknown <Yunus@MSI-A01341.mps.k12>
2022-09-08 12:03:42 -0800

    hello

--- js-basics/index.js
+++ js-basics/index.js
@@ -8,12 +8,37 @@
     // Cannot start with a number (1name)
     // Cannot contain a space or hyphen (-)
     // Aere case-sensitive 
-
+/*
     let person = {
         name: "Yunus",
-        age: 16
+        age: 16 
     };
 
-    console.log(person) ; 
+    // Dot Notations
+    person.name = "Yunus";
+    console.log(person.name) ; 
 
-    
+    //Bracket Notations
+    person["name"] = "name";
+    person[selection] = "Yunus";
+    console.log(person.name);
+
+    let selectedColors = ['red', 'blue']
+    selectedColors[2] = 1;
+    console.log(selectedColors.length);
+    function greeet () {
+        console.log('Hello World');
+    }
+
+    greet ("World")
+*/
+    function greet(name) {
+    console.log('hello ' + name);
+    }
+    greet('Yunus', 'Kilicalp');
+
+    function best(yourName){
+        console.log(yourName + ' is the best programmmer.');
+    }
+    best('Yunus');
+  
